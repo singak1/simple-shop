a3acb601ebf4f142e58fd2a0c4f7ce534c6436d1
singak1 <a97sin@gmail.com>
2023-03-30 10:45:36 -0400

    Update home.php

--- public_html/project/home.php
+++ public_html/project/home.php
@@ -1,14 +1,20 @@
 <?php
-require(__DIR__ . "/../../partials/nav.php");
+require(__DIR__."/../../partials/nav.php");
 ?>
 <h1>Home</h1>
 <?php
-if (is_logged_in()) {
-    //flash("Welcome, " . get_user_email(");
-} else {
+if(is_logged_in()){
+   // flash("Welcome, " . get_user_email());
+}
+else{
     flash("You're not logged in");
 }
-//shows session info
-flash("<pre>" . var_export($_SESSION, true) . "</pre>");
+/*if(isset($_SESSION["user"]) && isset($_SESSION["user"]["email"])){
+ echo "Welcome, " . $_SESSION["user"]["email"]; 
+}
+else{
+  echo "You're not logged in";
+}*/
+
+require(__DIR__."/../../partials/flash.php");
 ?>
-<?php require_once(__DIR__."/../../partials/flash.php");
