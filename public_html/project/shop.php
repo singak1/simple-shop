d43fd2a77297955ce7b43d3e0f5e44d67e304d74
singak1 <a97sin@gmail.com>
2023-04-21 15:17:30 -0400

    Add filter for category

--- public_html/project/shop.php
+++ public_html/project/shop.php
@@ -3,7 +3,27 @@
 require(__DIR__. "/../../partials/nav.php");
 $results = [];
 $db = getDB();
+$categoryFilter = $_GET['category'] ?? '';
 $stmt = $db->prepare("SELECT id, name, description, category, stock, unit_price FROM Products WHERE visibility = 'true' LIMIT 10");
+try {
+    $stmt->execute();
+    $r = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    if($r) {
+        $results = $r;
+    }
+} catch(PDOException $e) {
+    error_log(var_export($e, true));
+    flash("Error fetching items", "danger");
+}
+
+$categoryFilter = $_GET['category'] ?? '';
+
+// Fetch items from the database with category filter
+$results = [];
+$db = getDB();
+$stmt = $db->prepare("SELECT id, name, description, category, stock, unit_price FROM Products WHERE visibility = 'true' 
+                      AND (:categoryFilter = '' OR category = :categoryFilter) LIMIT 10");
+$stmt->bindParam(':categoryFilter', $categoryFilter);
 try {
     $stmt->execute();
     $r = $stmt->fetchAll(PDO::FETCH_ASSOC);
@@ -18,7 +38,22 @@ try {
 <script>
     //TODO ADD CART LOGIC HERE
 </script>
-
+<div class="container-fluid row justify-content-end col-md-3 ms-auto">
+    <form method="GET" action="shop.php">
+        <label for="category">Filter by Category:</label>
+        <select id="category" name="category" onchange="this.form.submit()" class="form-control">
+            <option value="">All</option>
+            <!-- Fetch and display available categories dynamically -->
+            <?php
+                $categories = array_unique(array_column($results, 'category'));
+                foreach ($categories as $category) {
+                    $selected = ($_GET['category'] ?? '') === $category ? 'selected' : '';
+                    echo "<option value='$category' $selected>$category</option>";
+                }
+            ?>
+        </select>
+    </form>
+</div>
 <div class="container-fluid">
     <h1>Shop</h1>
     <div class="row row-cols-1 row-cols-md-5 g-4">
@@ -33,7 +68,7 @@ try {
                         <p class="card-text">Description: <?php se($item, "description"); ?></p>
                     </div>
                     <div class="card-footer">
-                        Cost: <?php se($item, "cost"); ?>
+                        Cost: <?php se($item, "unit_price"); ?>
                         <button onclick="purchase('<?php se($item, 'id'); ?>')" class="btn btn-primary">Buy Now</button>
                     </div>
                 </div>
