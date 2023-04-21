38e5bf7b1f639d072af46ab6aca7d37f52a9851c
singak1 <a97sin@gmail.com>
2023-04-21 14:06:34 -0400

    Fix more bugs with UI after bootstrap

--- partials/nav.php
+++ partials/nav.php
@@ -38,7 +38,6 @@ session_start();
         <div class="collapse navbar-collapse" id="navContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <?php if (is_logged_in()) : ?>
-                    <li class="nav-item"><a class="nav-link" href="<?php echo get_url('home.php'); ?>">Home</a></li>
                     <li class="nav-item"><a class="nav-link" href="<?php echo get_url('profile.php'); ?>">Profile</a></li>
                 <?php endif; ?>
                 <?php if (!is_logged_in()) : ?>
@@ -71,7 +70,7 @@ session_start();
                     </li>
                 <?php endif; ?>
                 <?php if (is_logged_in()) : ?>
-                    <li class="nav-item"><a class="nav-item" href="<?php echo get_url('logout.php'); ?>">Logout</a></li>
+                    <li class="nav-item"><a class="nav-link" href="<?php echo get_url('logout.php'); ?>">Logout</a></li>
                 <?php endif; ?>
             </ul>
         </div>
