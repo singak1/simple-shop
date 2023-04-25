423f622a1ceed80eef79a0f9bd635a84391b2a37
singak1 <a97sin@gmail.com>
2023-04-25 12:03:15 -0400

    Fix url bug containing extra '/'

--- lib/functions.php
+++ lib/functions.php
@@ -3,7 +3,7 @@
 require_once(__DIR__ . "/db.php");
 //This is going to be a helper for redirecting to our base project path since it's nested in another folder
 //This MUST match the folder name exactly
-$BASE_PATH = '/project/';
+$BASE_PATH = '/project';
 //TODO 4: Flash Message Helpers
 //we moved the flash require higher so following functions have access to it
 require(__DIR__ . "/flash_messages.php");
