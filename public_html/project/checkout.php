bb1e9a66ac41a395be1a816b656813a0a2f504bf
singak1 <a97sin@gmail.com>
2023-05-02 14:59:11 -0400

    Add a checkout form

--- public_html/project/checkout.php
+++ public_html/project/checkout.php
@@ -92,6 +92,56 @@ try {
         <a class="btn btn-primary" href="cart.php" >Back to Cart</a>
     </div>
 </div>
+
+<div class="container-fluid mt-5">
+    <h3>Payment and Shipping Information</h3>
+    <form method="POST" action="purchase_cart.php">
+        <div class="form-group">
+            <label for="payment-method">Payment Method</label>
+            <select class="form-control" id="payment-method" name="payment_method">
+                <option value="cash">Cash</option>
+                <option value="visa">Visa</option>
+                <option value="mastercard">MasterCard</option>
+                <option value="amex">Amex</option>
+            </select>
+            <label for="payment-amount">Payment Amount</label>
+            <input type="number" class="form-control" id="payment-amount" name="payment_amount" value="<?php echo se($total_cost, null, 0); ?>" required>
+      </div>
+      <div class="form-group">
+            <label for="shipping-first-name">First Name</label>
+            <input type="text" class="form-control" id="shipping-first-name" name="shipping_first_name" required>
+      </div>
+      <div class="form-group">
+            <label for="shipping-last-name">Last Name</label>
+            <input type="text" class="form-control" id="shipping-last-name" name="shipping_last_name" required>
+      </div>
+      <div class="form-group">
+            <label for="shipping-address">Address</label>
+            <input type="text" class="form-control" id="shipping-address" name="shipping_address" required>
+      </div>
+      <div class="form-group">
+            <label for="shipping-apt-suite-fl">Apt./Suite/Fl</label>
+            <input type="text" class="form-control" id="shipping-apt-suite-fl" name="shipping_apt_suite_fl">
+      </div>
+      <div class="form-group">
+            <label for="shipping-city">City</label>
+            <input type="text" class="form-control" id="shipping-city" name="shipping_city" required>
+      </div>
+      <div class="form-group">
+            <label for="shipping-state-province">State/Province</label>
+            <input type="text" class="form-control" id="shipping-state-province" name="shipping_state_province" required>
+      </div>
+      <div class="form-group">
+            <label for="shipping-country">Country</label>
+            <input type="text" class="form-control" id="shipping-country" name="shipping_country" required>
+      </div>
+      <div class="form-group">
+            <label for="shipping-zip-postal-code">ZIP/Postal Code</label>
+            <input type="text" class="form-control" id="shipping-zip-postal-code" name="shipping_zip_postal_code" required>
+      </div>
+        <button type="submit" class="btn btn-primary mt-3">Submit</button>
+    </form>
+</div>
 <?php
 require(__DIR__ . "/../../partials/footer.php");
 ?>
