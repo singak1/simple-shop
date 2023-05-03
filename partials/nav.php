d6646cf04dd7d64d4b35fa74e8448db15a962d45
singak1 <a97sin@gmail.com>
2023-05-03 16:05:24 -0400

    Add purchase history and order details pages

--- partials/nav.php
+++ partials/nav.php
@@ -39,6 +39,7 @@ session_start();
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <?php if (is_logged_in()) : ?>
                     <li class="nav-item"><a class="nav-link" href="<?php echo get_url('profile.php'); ?>">Profile</a></li>
+                    <li class="nav-item"><a class="nav-link" href="<?php echo get_url('purchase_history.php'); ?>">Purchase History</a></li>
                 <?php endif; ?>
                 <?php if (!is_logged_in()) : ?>
                     <li class="nav-item"><a class="nav-link" href="<?php echo get_url('login.php'); ?>">Login</a></li>
